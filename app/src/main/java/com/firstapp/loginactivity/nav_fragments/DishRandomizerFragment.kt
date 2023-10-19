aab94ffb7adf36fceeac92869e55164c7b7dfa66
Veane <caramatmharvin@gmail.com>
2023-10-19 16:11:45 +0800

    Dish randomizer TEST

--- app/src/main/java/com/firstapp/loginactivity/nav_fragments/DishRandomizerFragment.kt
+++ app/src/main/java/com/firstapp/loginactivity/nav_fragments/DishRandomizerFragment.kt
@@ -1,60 +1,55 @@
 package com.firstapp.loginactivity.nav_fragments
 
 import android.os.Bundle
+import kotlin.random.Random
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import com.firstapp.loginactivity.R
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DishRandomizerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DishRandomizerFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var imageView: ImageView
+    private lateinit var rdmButton: Button
+    val foodImages: IntArray = intArrayOf(
+        R.drawable.adobochicken,
+        R.drawable.adobopork,
+        R.drawable.sisigpork
+    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_randomizer, container, false)
+        val root = inflater.inflate(R.layout.fragment_dish_randomizer, container, false)
+
+        imageView = root.findViewById(R.id.imageView)
+        rdmButton = root.findViewById(R.id.rdmButton)
+
+        imageSource()
+
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DishRandomizerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DishRandomizerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun imageSource() {
+        rdmButton.setOnClickListener {
+            val random = Random
+            val randomImageResource = foodImages[random.nextInt(foodImages.size)]
+            imageView.setImageResource(randomImageResource)
+        }
     }
+
 }
+
+
+
