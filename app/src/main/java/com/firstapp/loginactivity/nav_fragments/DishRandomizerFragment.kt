9461090476e27a72c5be4f636533d69585e38629
Veane <caramatmharvin@gmail.com>
2023-10-19 16:11:45 +0800

    Dish randomizer TEST

--- app/src/main/java/com/firstapp/loginactivity/nav_fragments/DishRandomizerFragment.kt
+++ app/src/main/java/com/firstapp/loginactivity/nav_fragments/DishRandomizerFragment.kt
@@ -11,7 +11,15 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.firstapp.loginactivity.R
 
+// TODO: Rename parameter arguments, choose names that match
+//the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
 class DishRandomizerFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
     private lateinit var imageView: ImageView
     private lateinit var rdmButton: Button
     private lateinit var continueButton: Button
