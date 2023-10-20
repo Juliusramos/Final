5ff9b9bdcb0b020ac9b8691239ebfeb2b613bb80
Jamessysystem <smenardjames@gmail.com>
2023-10-20 14:18:39 +0800

    for saving trying adobo trying complete(?) layout

--- app/src/main/java/com/firstapp/loginactivity/bottom_fragments/PorkAdobo.kt
+++ app/src/main/java/com/firstapp/loginactivity/bottom_fragments/PorkAdobo.kt
@@ -5,14 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.firstapp.loginactivity.R
 
 class PorkAdobo : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -23,9 +19,6 @@ class PorkAdobo : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_porkadobo, container, false)
-        val adobosagata = view.findViewById<TextView>(R.id.tv_adobosagata)
-        val bulletedText = getString(R.string.bl_adobosagata)
-        adobosagata.text = bulletedText
         // Inflate the layout for this fragment
         return view
     }
