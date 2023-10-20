244c801e4ca6583b81d3c018e097f15389c07113
Jamessysystem <smenardjames@gmail.com>
2023-10-20 19:04:09 +0800

    Fixed layout and then added a vector

--- app/src/main/java/com/firstapp/loginactivity/dishes/AdobosaGata.kt
+++ app/src/main/java/com/firstapp/loginactivity/dishes/AdobosaGata.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.firstapp.loginactivity.R
+import com.firstapp.loginactivity.bottom_fragments.Home2Fragment
 
 class AdobosaGata : Fragment() {
     
@@ -18,7 +20,7 @@ class AdobosaGata : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_porkadobo, container, false)
+        val view = inflater.inflate(R.layout.fragment_adobosagata, container, false)
         // Inflate the layout for this fragment
         return view
     }
