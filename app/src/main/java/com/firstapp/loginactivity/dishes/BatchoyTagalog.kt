1ad9e10c57f4d629ea2fe74273178c8581b111c7
Veane <caramatmharvin@gmail.com>
2023-10-22 17:29:41 +0800

    Dishrandomizer done

--- app/src/main/java/com/firstapp/loginactivity/dishes/BatchoyTagalog.kt
+++ app/src/main/java/com/firstapp/loginactivity/dishes/BatchoyTagalog.kt
@@ -1,14 +1,12 @@
 package com.firstapp.loginactivity.dishes
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.firstapp.loginactivity.R
-import com.firstapp.loginactivity.bottom_fragments.Home2Fragment
 
 class BatchoyTagalog : Fragment() {
     override fun onCreateView(
